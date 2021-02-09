79f941fcf6db5e41808efa64f5a9527bdb739c99
adamstelmaszyk <sas23@poczta.fm>
2021-02-09 09:10:15 +0100

    Dodanie komentarza do pliku FileSeparator

--- FileSeparator.java
+++ FileSeparator.java
@@ -3,6 +3,8 @@ import java.io.*; //* - wszystkie clasy/biblioteki z java.io
 
 class FileSeparator{
 	public static void main(String [] a){
+	//File.separator - systemowy separator folderów
+	//czyli \ - windows    / - linux/macos
         String path = File.separator+"src"+File.separator+
             "main"+File.separator;
         System.out.println(path);
