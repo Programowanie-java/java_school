f4b040dd36f15e269ba1b06a31d6efe268bc7997
adamstelmaszyk <sas23@poczta.fm>
2021-02-09 09:05:01 +0100

    Jak użyć systemowego separatora folderów \ czy /

---
+++ FileSeparator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.*; //* - wszystkie clasy/biblioteki z java.io
+
+class FileSeparator{
+	public static void main(String [] a){
+        String path = File.separator+"src"+File.separator+
+            "main"+File.separator;
+        System.out.println(path);
+
+	}
+}
